3b1bfb30d66b0273b1bfce56d0a1aec5c2cca617
Demi-urge <158392389+Demi-urge@users.noreply.github.com>
2025-09-04 23:30:47 +1000

    Deprecate state_snapshot in favor of snapshot_tracker

--- self_improvement/__init__.py
+++ self_improvement/__init__.py
@@ -4,3 +4,8 @@ from __future__ import annotations
 
 from .api import *  # noqa: F401,F403
 from .api import __all__  # noqa: F401
+
+# Backwards compatibility for the deprecated `state_snapshot` module
+from . import snapshot_tracker as state_snapshot  # noqa: F401
+import sys as _sys
+_sys.modules[__name__ + ".state_snapshot"] = state_snapshot
