833fdcbe12279f7bd56f9eb78934f5ba12a8eea8
Demi-urge <158392389+Demi-urge@users.noreply.github.com>
2026-01-30 14:02:55 +1000

    Add stabilization logging pipeline

--- menace_sandbox/stabilization/__init__.py
+++ menace_sandbox/stabilization/__init__.py
@@ -1,5 +1,5 @@
 """Stabilization utilities for normalizing LLM outputs."""
 
-from .pipeline_wrapper import stabilize_completion
+from .pipeline import stabilize_completion
 
 __all__ = ["stabilize_completion"]
